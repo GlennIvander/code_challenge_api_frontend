3785a73d5f3def87736343b624c6f59233e5b79b
GlennIvander <ivander792@gmail.com>
2025-06-25 18:53:58 +0800

    added routes, 7

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -27,13 +27,13 @@ const features = [
         xmlns="http://www.w3.org/2000/svg"
         fill="none"
         viewBox="0 0 24 24"
-        stroke-width="1"
+        strokeWidth="1"
         stroke="currentColor"
-        class="size-16"
+        className="size-16"
       >
         <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeLinecap="round"
+          strokeLinejoin="round"
           d="m15.75 10.5 4.72-4.72a.75.75 0 0 1 1.28.53v11.38a.75.75 0 0 1-1.28.53l-4.72-4.72M4.5 18.75h9a2.25 2.25 0 0 0 2.25-2.25v-9a2.25 2.25 0 0 0-2.25-2.25h-9A2.25 2.25 0 0 0 2.25 7.5v9a2.25 2.25 0 0 0 2.25 2.25Z"
         />
       </svg>
@@ -47,13 +47,13 @@ const features = [
         xmlns="http://www.w3.org/2000/svg"
         fill="none"
         viewBox="0 0 24 24"
-        stroke-width="1"
+        strokeWidth="1"
         stroke="currentColor"
-        class="size-16"
+        className="size-16"
       >
         <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeLinecap="round"
+          strokeLinejoin="round"
           d="M8.625 9.75a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H8.25m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H12m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0h-.375m-13.5 3.01c0 1.6 1.123 2.994 2.707 3.227 1.087.16 2.185.283 3.293.369V21l4.184-4.183a1.14 1.14 0 0 1 .778-.332 48.294 48.294 0 0 0 5.83-.498c1.585-.233 2.708-1.626 2.708-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0 0 12 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018Z"
         />
       </svg>
@@ -67,13 +67,13 @@ const features = [
         xmlns="http://www.w3.org/2000/svg"
         fill="none"
         viewBox="0 0 24 24"
-        stroke-width="1"
+        strokeWidth="1"
         stroke="currentColor"
-        class="size-16"
+        className="size-16"
       >
         <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeLinecap="round"
+          strokeLinejoin="round"
           d="M18 18.72a9.094 9.094 0 0 0 3.741-.479 3 3 0 0 0-4.682-2.72m.94 3.198.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0 1 12 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 0 1 6 18.719m12 0a5.971 5.971 0 0 0-.941-3.197m0 0A5.995 5.995 0 0 0 12 12.75a5.995 5.995 0 0 0-5.058 2.772m0 0a3 3 0 0 0-4.681 2.72 8.986 8.986 0 0 0 3.74.477m.94-3.197a5.971 5.971 0 0 0-.94 3.197M15 6.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm6 3a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Zm-13.5 0a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Z"
         />
       </svg>
@@ -87,13 +87,13 @@ const features = [
         xmlns="http://www.w3.org/2000/svg"
         fill="none"
         viewBox="0 0 24 24"
-        stroke-width="1"
+        strokeWidth="1"
         stroke="currentColor"
-        class="size-16"
+        className="size-16"
       >
         <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeLinecap="round"
+          strokeLinejoin="round"
           d="M9 17.25v1.007a3 3 0 0 1-.879 2.122L7.5 21h9l-.621-.621A3 3 0 0 1 15 18.257V17.25m6-12V15a2.25 2.25 0 0 1-2.25 2.25H5.25A2.25 2.25 0 0 1 3 15V5.25m18 0A2.25 2.25 0 0 0 18.75 3H5.25A2.25 2.25 0 0 0 3 5.25m18 0V12a2.25 2.25 0 0 1-2.25 2.25H5.25A2.25 2.25 0 0 1 3 12V5.25"
         />
       </svg>
@@ -107,13 +107,13 @@ const features = [
         xmlns="http://www.w3.org/2000/svg"
         fill="none"
         viewBox="0 0 24 24"
-        stroke-width="1"
+        strokeWidth="1"
         stroke="currentColor"
-        class="size-16"
+        className="size-16"
       >
         <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeLinecap="round"
+          strokeLinejoin="round"
           d="M16.5 18.75h-9m9 0a3 3 0 0 1 3 3h-15a3 3 0 0 1 3-3m9 0v-3.375c0-.621-.503-1.125-1.125-1.125h-.871M7.5 18.75v-3.375c0-.621.504-1.125 1.125-1.125h.872m5.007 0H9.497m5.007 0a7.454 7.454 0 0 1-.982-3.172M9.497 14.25a7.454 7.454 0 0 0 .981-3.172M5.25 4.236c-.982.143-1.954.317-2.916.52A6.003 6.003 0 0 0 7.73 9.728M5.25 4.236V4.5c0 2.108.966 3.99 2.48 5.228M5.25 4.236V2.721C7.456 2.41 9.71 2.25 12 2.25c2.291 0 4.545.16 6.75.47v1.516M7.73 9.728a6.726 6.726 0 0 0 2.748 1.35m8.272-6.842V4.5c0 2.108-.966 3.99-2.48 5.228m2.48-5.492a46.32 46.32 0 0 1 2.916.52 6.003 6.003 0 0 1-5.395 4.972m0 0a6.726 6.726 0 0 1-2.749 1.35m0 0a6.772 6.772 0 0 1-3.044 0"
         />
       </svg>
--- src/pages/Authentication.jsx
+++ src/pages/Authentication.jsx
@@ -1,12 +1,96 @@
 import PropTypes from "prop-types";
+import { useState } from "react";
+import { validateEmail, validatePassword } from "../utilities/validation";
+import { Link } from "react-router-dom";
+
+const InitialErrorsState = {
+  email: "",
+  password: "",
+  api: "",
+};
 
 const Authentication = ({ pageType }) => {
+  const [email, setEmail] = useState(null);
+  const [password, setPassword] = useState(null);
+  const [errors, setErrors] = useState(InitialErrorsState);
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = async (e) => {
+    if (!validateEmail(email)) {
+    }
+    setErrors({
+      ...errors,
+      email: "Invalid email",
+    });
+    if (!validatePassword(password)) {
+    }
+    setErrors({
+      ...errors,
+      password: "Password should be atleast 6 characters long",
+    });
+  };
+
   return (
     <div className="bg-white">
       <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 py-12">
         <h3 className="text-xl font-bold">
           {pageType === PageType.LOGIN ? "Login" : "Register"}
         </h3>
+        {
+          (pageType === PageType.LOGIN) ?
+            <p>
+            Not a user?
+            <Link to="/register" className="ms-1 underline">
+              Register
+            </Link>
+            </p>
+            :
+            <p>
+            Already a user?
+            <Link to="/login" className="ms-1 underline">
+              Login
+            </Link>
+            </p>
+        }
+        <form onSubmit={handleSubmit}>
+          <div className="mt-10 flex gap-4 flex flex-col max-w-90">
+            <input
+              name="email"
+              type="email"
+              className="py-2 border border-gray-600 rounded px-3"
+              placeholder="enter email"
+              value={email}
+              onChange={handleEmailChange}
+            />
+            {errors.email && (
+              <p className="text-sm text-red-500 mt-1">{errors.email}</p>
+            )}
+            <input
+              name="password"
+              type="password"
+              className="py-2 border border-gray-600 rounded px-3"
+              placeholder="enter password"
+              value={password}
+              onChange={handlePasswordChange}
+            />
+            {errors.password && (
+              <p className="text-sm text-red-500 mt-1">{errors.password}</p>
+            )}
+            <button
+              type="submit"
+              className="bg-indigo-500 hover:bg-indigo-700 px-3 py-2 rounded text-white"
+            >
+              {pageType === PageType.LOGIN ? "Login" : "Register"}
+            </button>
+          </div>
+        </form>
       </div>
     </div>
   );
