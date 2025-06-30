04109b22aa4991e3104a880cec5406df06fb1b1c
GlennIvander <ivander792@gmail.com>
2025-06-30 23:55:39 +0800

    added children

--- src/elements/Button.jsx
+++ src/elements/Button.jsx
@@ -1,12 +1,11 @@
-const Button = () => {
+const Button = ({children}) => {
   return (
     <div className="bg-indigo-300 rounded hover:-translate-x-0.5 hover:-translate-y-0.5">
       <button
         type="submit"
         className="w-full bg-indigo-300 text-black hover:-translate-x-1.5 hover:-translate-y-1.5 hover:bg-indigo-600 hover:text-white px-3 py-2 rounded"
       >
-        {/* {pageType === PageType.LOGIN ? "Login" : "Register"} */}
-        Login
+        {children}
       </button>
     </div>
   );
--- src/pages/Authentication.jsx
+++ src/pages/Authentication.jsx
@@ -139,16 +139,10 @@ const Authentication = ({ pageType }) => {
               <p className="text-sm text-red-500 mt-1">{errors.password}</p>
             )}
 
-            <Button />
+            <Button>
+              {pageType === PageType.LOGIN ? "Login" : "Register"}
+            </Button>
 
-            <div className="bg-indigo-300 rounded hover:-translate-x-0.5 hover:-translate-y-0.5">
-              <button
-                type="submit"
-                className="w-full bg-indigo-300 text-black hover:-translate-x-1.5 hover:-translate-y-1.5 hover:bg-indigo-600 hover:text-white px-3 py-2 rounded"
-              >
-                {pageType === PageType.LOGIN ? "Login" : "Register"}
-              </button>
-            </div>
           </div>
         </form>
       </div>
